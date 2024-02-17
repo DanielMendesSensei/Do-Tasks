8334ac31c62d11af5f61ab76f615aab23b587a99
Daniel Mendes <senseidanielmendes@gmail.com>
2024-02-16 23:31:41 -0300

    build: Do Tasks! version 1.0

---
+++ .github/README.md
@@ -0,0 +1,15 @@
+# Sidebar Menu
+😃Check out my oldest simple project of taks manager
+
+### Version 1.0
+
+<!-- ## The website is ready! Tap the image below or this [link](https://danielmendessensei.github.io/SidebarMenu/) to access -->
+
+<!-- # Screenshots:
+[![Screenshot](assets/img/1.png 'Tap the image to access')](https://danielmendessensei.github.io/SidebarMenu/)  
+
+[![Screenshot](assets/img/2.png 'Tap the image to access')](https://danielmendessensei.github.io/SidebarMenu/) 
+
+[![Screenshot](assets/img/4.png 'Tap the image to access')]https://danielmendessensei.github.io/SidebarMenu/) 
+
+[![Screenshot](assets/img/3.png 'Tap the image to access')](https://danielmendessensei.github.io/SidebarMenu/)  -->
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,36 @@
-// document.querySelector('#push').onclick = ()=>{
-//     let input = document.getElementById('input').value
-//     if(!input){
-//         alert("Please Enter a Task")
-//     } else
-//     {
-        
-//         let tasks = document.getElementById('tasks')
-//         tasks.addEventListener('click', ()=>{
-
-//         })
-//     }
-// }
-const tasks = document.getElementById('tasks')
-const task = document.getElementById('task')
+const tasks = document.querySelector('.tasks')
+// const task = document.getElementById('task')
 const pushButton = document.getElementById('push')
+
 pushButton.addEventListener('click', ()=>{
-    console.log("AQUI")
-    let input = document.getElementById('input').value
-    if(!input){
+    let input = document.getElementById('input')
+    // console.log(input.value)
+    if(!input.value){
         alert("Please Enter a Task")
-    } else
-    {
-        task.style.display = 'block';
-        // tasks.classList.add('task')
+    } else {
+        tasks.innerHTML += `<div class="task">
+                            <span id="taskname">
+                                ${input.value}  
+                            </span>
+                            <button class="delete">
+                                <ion-icon name="trash-outline"></ion-icon>
+                            </button>
+                        </div>`
+        let deleteTasks = document.querySelectorAll('.delete')
+        for(let i = 0; i < deleteTasks.length; i++){
+            let deleteTask = deleteTasks[i]
+            deleteTask.onclick = ()=>{
+                deleteTask.parentNode.remove()
+                
+            }}
+        let currentTasks = document.querySelectorAll('.task')
+        for (let i = 0; i < currentTasks.length; i++){
+            let currentTask = currentTasks[i]
+            currentTask.onclick = ()=>{
+                currentTask.classList.add('completed')
+            }
+        }
+        input.value = "";
     }
 });
+
