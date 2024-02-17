1e56c057306ca42d3b7c15138fb1584ddcb5f2cf
Daniel Mendes <senseidanielmendes@gmail.com>
2024-02-17 12:43:36 -0300

    feat: New features for tasks, and a new UI/UX

--- .github/README.md
+++ .github/README.md
@@ -1,25 +1,25 @@
 # Do Tasks
 😃Check out my oldest simple project of taks manager
 
-### 📤 Last Update:  v0.1.2, refactor README
+### 📤 Last Update:  v0.2.0, feat: New features for tasks, like adding tasks by pressing Enter, verifying if task already exists, and a new UI/UX with responsive layout
 
 ## 💻 The website is ready! Tap in this [link](https://danielmendessensei.github.io/Do-Tasks/) to access -->
 
 ## 🤔 How to use
 Intuitively, you type the task you want and click on ADD. This will add your task to the container below, as shown in these images:
 
-[![Screenshot](../assets/img/1.png)]
-[![Screenshot](../assets/img/2.png)]
+[Screenshot](../assets/img/1.png)
+[Screenshot](../assets/img/2.png)
 
 If you have completed the task, click on it, so the task will have a line through, indicating that it has been completed.
 
-[![Screenshot](../assets/img/3.png)]
+[Screenshot](../assets/img/3.png)
 
 you also can delete the task, by just clicking the icon: <img src="../assets/img/4.png" width=75px height=60px/>
 
 ## 📸 Screenshots
-[![Screenshot](../assets/img/1.png)]
-[![Screenshot](../assets/img/5.png)]
+[Screenshot](../assets/img/1.png)
+[Screenshot](../assets/img/5.png)
 
 ## 🚀 Stack
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,64 @@
-const tasks = document.querySelector('.tasks')
-// const task = document.getElementById('task')
+// DOM elements
 const pushButton = document.getElementById('push')
+pushButton.addEventListener('click', Tasks)
 
-pushButton.addEventListener('click', ()=>{
-    let input = document.getElementById('input')
-    // console.log(input.value)
-    if(!input.value){
+// Call Tasks by press enter
+document.addEventListener('keydown', (event)=>{
+    if (event.key === "Enter"){
+        Tasks()
+    }
+})
+
+// Functions
+let tasksSet = new Set();
+function Tasks(){
+    const tasks = document.querySelector('.tasks')
+    const input = document.getElementById('input')
+    let taskName = input.value.trim()
+
+    // Task is null?
+    if(!taskName){
         alert("Please Enter a Task")
-    } else {
+        return
+    } 
+
+    // Task already exists?
+    if (tasksSet.has(taskName)){
+        alert("Task already exists!") 
+        return
+    }
+
+    // Add task to the container
+    else {
         tasks.innerHTML += `<div class="task">
-                            <span id="taskname">
-                                ${input.value}  
-                            </span>
+                            <span class="taskname">${taskName}</span>
                             <button class="delete">
                                 <ion-icon name="trash-outline"></ion-icon>
                             </button>
-                        </div>`
-        let deleteTasks = document.querySelectorAll('.delete')
-        for(let i = 0; i < deleteTasks.length; i++){
-            let deleteTask = deleteTasks[i]
-            deleteTask.onclick = ()=>{
-                deleteTask.parentNode.remove()
-                
-            }}
-        let currentTasks = document.querySelectorAll('.task')
-        for (let i = 0; i < currentTasks.length; i++){
-            let currentTask = currentTasks[i]
-            currentTask.onclick = ()=>{
-                currentTask.classList.add('completed')
-            }
-        }
-        input.value = "";
+                        </div>`;
+        tasksSet.add(taskName) //Add task to set
     }
-});
+
+    // Deleting tasks
+    let deleteTasks = document.querySelectorAll('.delete')
+    for(let i = 0; i < deleteTasks.length; i++){
+        let deleteTask = deleteTasks[i]
+        deleteTask.onclick = ()=>{
+            tasksSet.clear() //Clear de set
+            deleteTask.parentNode.remove()
+        };
+    };
+
+    // Mark tasks as Done    
+    let currentTasks = document.querySelectorAll('.task')
+    for (let i = 0; i < currentTasks.length; i++){
+        let currentTask = currentTasks[i]
+        currentTask.onclick = ()=>{
+            currentTask.classList.add('completed')
+        };
+    };
+
+    // Clear input value
+    input.value = "";
+};
 
